992b4c99233f2fb799afc1fdd58ebf7a2a0d61b2
Rafael Machado <rafael.machado@dp6.com.br>
2024-01-26 10:08:51 -0300

    model imports

---
+++ model.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain.retrievers.self_query.base import SelfQueryRetriever
+from langchain.chains.query_constructor.base import AttributeInfo
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema.output_parser import StrOutputParser
+from langchain.memory import ConversationTokenBufferMemory
+from langchain_core.prompts import MessagesPlaceholder
+from langchain_openai.embeddings import OpenAIEmbeddings
+from langchain_openai.chat_models import ChatOpenAI
+from langchain_community.document_loaders import DirectoryLoader
+from langchain_community.vectorstores import Milvus
+from milvus import default_server as milvus_server
