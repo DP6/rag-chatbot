d95ddebf0521c4379610985a27a109bb050a5b54
Rafael Machado <rafael.machado@dp6.com.br>
2024-01-26 10:13:41 -0300

    private methods

--- model.py
+++ model.py
@@ -25,3 +25,54 @@ class RAG():
         self.__docs_list = self.__get_docs_list(docs_dir)
         self.__retriever = self.__set_retriever(k=n_retrievals)
         self.__chat_history = self.__set_chat_history(max_token_limit=chat_max_tokens)
+
+
+    # PRIVATE METHODS #
+    def __set_llm_model(self, model_name = "gpt-3.5-turbo", temperature: float = 0.7):
+        return ChatOpenAI(model_name=model_name, temperature=temperature)
+    
+    def __get_docs_list(self, docs_dir: str) -> list:
+        print("Carregando documentos...")
+        loader = DirectoryLoader(docs_dir,
+                                 recursive=True,
+                                 show_progress=True,
+                                 use_multithreading=True,
+                                 max_concurrency=4)
+        docs_list = loader.load_and_split()
+       
+        return docs_list
+    
+    def __set_retriever(self, k: int = 4):
+        # Milvus Vector Store
+        embeddings = OpenAIEmbeddings()
+        milvus_server.start()
+        vector_store = Milvus.from_documents(
+            self.__docs_list,
+            embedding=embeddings,
+            connection_args={"host": os.getenv("MILVUS_HOST"), "port": os.getenv("MILVUS_PORT")},
+            collection_name="personal_documents",
+        )
+
+        # Self-Querying Retriever
+        metadata_field_info = [
+            AttributeInfo(
+                name="source",
+                description="O caminho de diretórios onde se encontra o documento",
+                type="string",
+            ),
+        ]
+
+        document_content_description = "Documentos pessoais"
+
+        _retriever = SelfQueryRetriever.from_llm(
+            self.__model,
+            vector_store,
+            document_content_description,
+            metadata_field_info,
+            search_kwargs={"k": k}
+        )
+
+        return _retriever
+    
+    def __set_chat_history(self, max_token_limit: int = 3097):
+        return ConversationTokenBufferMemory(llm=self.__model, max_token_limit=max_token_limit, return_messages=True)
