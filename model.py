5bdb86d2674e71416ec2b1ee54e72d74267e0c41
Rafael Machado <rafael.machado@dp6.com.br>
2024-01-26 10:09:43 -0300

    RAG class definition

--- model.py
+++ model.py
@@ -13,3 +13,15 @@ from langchain_openai.chat_models import ChatOpenAI
 from langchain_community.document_loaders import DirectoryLoader
 from langchain_community.vectorstores import Milvus
 from milvus import default_server as milvus_server
+
+class RAG():
+    def __init__(self,
+                 docs_dir: str,
+                 n_retrievals: int = 4,
+                 chat_max_tokens: int = 3097,
+                 model_name = "gpt-3.5-turbo",
+                 creativeness: float = 0.7):
+        self.__model = self.__set_llm_model(model_name, creativeness)
+        self.__docs_list = self.__get_docs_list(docs_dir)
+        self.__retriever = self.__set_retriever(k=n_retrievals)
+        self.__chat_history = self.__set_chat_history(max_token_limit=chat_max_tokens)
